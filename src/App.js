e639c108025a55bee54d0e2d4403fc657258099d
wispyplant <149518124+wispyplant@users.noreply.github.com>
2023-11-30 17:01:41 +0000

    add gui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
-}
+// Create a title element
+const title = document.createElement('h1');
+title.textContent = 'Image Analysis and Generation';
+
+// Create a text box for image URL or prompt
+const input = document.createElement('input');
+input.type = 'text';
+input.placeholder = 'Enter image URL or prompt';
+
+// Create a button for image analysis
+const analyzeButton = document.createElement('button');
+analyzeButton.textContent = 'Analyze Image';
+
+// Create a button for image generation
+const generateButton = document.createElement('button');
+generateButton.textContent = 'Generate Image';
+
+// Append elements to the document body
+document.body.appendChild(title);
+document.body.appendChild(input);
+document.body.appendChild(analyzeButton);
+document.body.appendChild(generateButton);
 
 export default App;
